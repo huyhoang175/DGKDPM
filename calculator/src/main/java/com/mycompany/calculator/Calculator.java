3929f38fd42c145eefae9f96903a24d97d2c7646
huyhoang175 <21011604@st.phenikaa-uni.edu.vn>
2024-04-23 22:13:14 +0700

    testjava

---
+++ calculator/src/main/java/com/mycompany/calculator/Calculator.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculator;
+
+/**
+ *
+ * @author admin
+ */
+public class Calculator {
+    public int add(int a, int b){
+    int rs = a + b;
+    return rs;
+    }
+    
+    public int sub(int a, int b){
+    int rs = a - b;
+    return rs;
+    }
+    
+    public int mul(int a, int b){
+    int rs = a * b;
+    return rs;
+    }
+    
+    public int div(int a, int b){
+    int rs = a / b;
+    return rs;
+    }
+}
